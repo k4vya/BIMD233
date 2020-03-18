b54b2ce576337cc5804dd918706c886743d3b4b0
k4vya <kavyai@uw.edu>
2020-03-18 16:10:32 -0700

    kms

---
+++ Labs/Lab18/18b/fader.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $('li').css('margin', '10px');
+  $('li').attr('id', 'uw');
+
+  $('#p1 li').click(function () {
+    console.log("$(this):" + $(this));
+    $(this).fadeOut(2000, function () {
+      console.log("fadeout complete!")
+    });
+  });
+
+  $('#p2 li').click(function () {
+    console.log("$(this):" + $(this));
+    $(this).fadeOut(2000, function () {
+      console.log("fadeout complete!")
+    });
+  });
+
+  $('#p3 li').click(function () {
+    console.log("$(this):" + $(this));
+    $(this).fadeOut(2000, function () {
+      console.log("fadeout complete!")
+    });
+  });
+});
